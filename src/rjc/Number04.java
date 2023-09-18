45d55f2e124c728516c314031084c3577ea27f75
katsuma0728 <katsuma20220728@gmail.com>
2023-09-18 19:44:29 +0900

    [add]comment

--- src/rjc/Number04.java
+++ src/rjc/Number04.java
@@ -1,6 +1,10 @@
 package rjc;
 
 public class Number04 {
+	
+	//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、
+	//[11]~[19]の値は偶数をカンマ区切りで表示させる
+
 	public static void main(String[] args) {
 		int[] number = new int[20];
 
